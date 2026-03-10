8cf153bf68902c5e415d5b054f3374f897929247
Pavel Halukha <pavelgaluha@gmail.com>
2026-03-10 21:56:10 +0300

    add get lesson use case

---
+++ lib/feature/lesson/domain/repository/lesson_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:easy_knowledge/core/error/failure.dart';
+import 'package:easy_knowledge/feature/lesson/domain/entity/lesson.dart';
+
+abstract class LessonRepository {
+  Future<Either<Failure, Lesson>> getLesson(String id);
+}
---
+++ lib/feature/lesson/domain/usecase/get_lesson.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:easy_knowledge/core/error/failure.dart';
+import 'package:easy_knowledge/feature/lesson/domain/entity/lesson.dart';
+import 'package:easy_knowledge/feature/lesson/domain/repository/lesson_repository.dart';
+
+class GetLesson {
+  GetLesson({required this._repository});
+
+  final LessonRepository _repository;
+
+  Future<Either<Failure, Lesson>> call(String id) {
+    return _repository.getLesson(id);
+  }
+}
